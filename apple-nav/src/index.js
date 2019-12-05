08026681c148ffe5c9df1bb411c847f47367be5a
JRodDvlpr <Mialienproductions@gmail.com>
2019-12-05 12:10:44 -0500

    Routed App in Index.js

--- apple-nav/src/index.js
+++ apple-nav/src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+const rootElement = document.getElementById('root');
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router>
+    <App />
+    </Router>,
+    rootElement
+);
 
