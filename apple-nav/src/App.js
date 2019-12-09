7a6456abf6ad47abcb9eb08107a5b3ba22118f15
JRodDvlpr <Mialienproductions@gmail.com>
2019-12-09 16:35:45 -0500

    Components Created

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavWrap from './components/NavWrapper';
 
 function App() {
   return (
     <div className="App">
+      <NavWrap />
      
     </div>
   );
---
+++ apple-nav/src/components/NavWrapper.js
@@ -0,0 +1,9 @@
+import React, {useState} from 'react';
+
+const NavWrap = () => {
+    const [menu, setMenu] = useState();
+
+    return;
+};
+
+export default NavWrap;
